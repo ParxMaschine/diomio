638986bc671ebef3f307e6bef8ded11fd733a70a
Richard Bruno 21 1591 <richard.huaman.27@unsch.edu.pe>
2023-08-21 09:31:55 -0500

    omg mvc

---
+++ DW_final_final/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using DW_final_final.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DW_final_final.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DW_final_final/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DW_final_final.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
